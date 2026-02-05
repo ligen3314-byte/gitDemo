8762230d118b572216e85809afe9ee93a961e7e5
ligen3314-byte <ligen3314@gmail.com>
2026-02-05 23:53:39 +0800

    modify main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+#设置http:
+git config --global http.proxy http://127.0.0.1:7890#设置https:
+git config --global https.proxy https://127.0.0.1:7890#设置socks:
+git config --global http.proxy 'socks5://127.0.0.1:7890'
+git config --global https.proxy 'socks5://127.0.0.1:7890'
